f44e79501405d68a29d43dc35b0b7d1fa7305d0c
dobbiedobbiedobbie <78974648+dobbiedobbiedobbie@users.noreply.github.com>
2021-02-12 16:06:10 -0500

    New GTM Update

---
+++ dobbieInfoTag.js
@@ -0,0 +1,15 @@
+(function () {
+    window._uxa = window._uxa || [];
+    try {
+
+    } catch (e){}
+    if (typeof CS_CONF === 'undefined') {
+      window._uxa.push(['setPath', window.location.pathname+window.location.hash.replace('#','?__')]);
+      var mt = document.createElement("script"); mt.type = "text/javascript"; mt.async = true;
+      mt.src = "//t.contentsquare.net/uxa/a30908616073b.js";
+      document.getElementsByTagName("head")[0].appendChild(mt);
+    }
+    else {
+      window._uxa.push(['trackPageview', window.location.pathname+window.location.hash.replace('#','?__')]);
+    }
+  })();
